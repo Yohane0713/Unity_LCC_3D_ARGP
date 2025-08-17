1cb3f9982fc474b8074546cb86dc226de9041dad
Yohane0713 <tea005454@gmail.com>
2025-08-17 19:55:29 +0800

    已完成存檔讀檔FireBase功能

---
+++ Assets/Scripts/DataPlayer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Mtaka
+{
+    /// <summary>
+    /// 玩家要儲存的資料
+    /// </summary>
+    [System.Serializable]
+    public class DataPlayer
+    {
+        public Vector3 position;
+        public Vector3 rotation;
+    }
+}
--- Assets/Scripts/SaveLoadSystem.cs
+++ Assets/Scripts/SaveLoadSystem.cs
@@ -1,5 +1,8 @@
+﻿using Firebase.Database;
+using StarterAssets;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Mtaka
 {
@@ -12,20 +15,34 @@ namespace Mtaka
         // 等待真實時間，不被時間暫停影響
         private WaitForSecondsRealtime waitFadeInterval = new WaitForSecondsRealtime(0.02f);
 
+        [SerializeField]
+        private DataPlayer player;
+        private Transform traPlayer;
+        private Button btnLoadData, btnSaveData;
+        private DatabaseReference dbReference;
+        private string dataName = "測試資料";
+        private ThirdPersonController controller;
+        private Animator ani;
+
         private void Awake()
         {
-            groupSaveLoad = GameObject.Find("").GetComponent<CanvasGroup>();
+            dbReference = FirebaseDatabase.DefaultInstance.RootReference;
+            traPlayer = GameObject.Find("玩家_Unity醬").transform;
+            controller = traPlayer.GetComponent<ThirdPersonController>();
+            ani = traPlayer.GetComponent<Animator>();
+            groupSaveLoad = GameObject.Find("群組_存讀檔畫面").GetComponent<CanvasGroup>();
+            btnLoadData = GameObject.Find("按鈕讀取檔案").GetComponent<Button>();
+            btnSaveData = GameObject.Find("按鈕儲存檔案").GetComponent<Button>();
+            btnLoadData.onClick.AddListener(Load);
+            btnSaveData.onClick.AddListener(Save);
         }
 
         private void Update()
         {
-            if (Input.GetKeyDown(KeyCode.Escape))
-            {
-                StartCoroutine(Fade());
-            }
+            ShowSaveLoad();
         }
 
-        private void SaveLoad()
+        private void ShowSaveLoad()
         {
             if (Input.GetKeyDown(KeyCode.Escape))
             {
@@ -46,6 +63,70 @@ namespace Mtaka
 
             groupSaveLoad.interactable = true;
             groupSaveLoad.blocksRaycasts = true;
+        }
+
+        private void Load()
+        {
+            StartCoroutine(LoadData());
+        }
+
+        private IEnumerator LoadData()
+        {
+            // 從資料庫獲得資料
+            var serverData = dbReference.Child(dataName).GetValueAsync();
+            // 等待直到伺服器下載資料完成
+            yield return new WaitUntil(() => serverData.IsCompleted);
+            // 儲存獲得的結果
+            DataSnapshot snapshot = serverData.Result;
+            // 將結果轉為JSON字串
+            string jsonData = snapshot.GetRawJsonValue();
+            // 將JSON字串轉為玩家資料
+            player = JsonUtility.FromJson<DataPlayer>(jsonData);
+            // 等待淡出
+            yield return StartCoroutine(Fadeout());
+            // 設定玩家
+            StartCoroutine(SetPlayer());
+        }
+
+        private void Save()
+        {
+            player.position = traPlayer.position;
+            player.rotation = traPlayer.eulerAngles;
+            string json = JsonUtility.ToJson(player);
+            print(json);
+            // 將Json資料儲存到資料庫的dataName下方
+            dbReference.Child(dataName).SetRawJsonValueAsync(json);
+            StartCoroutine(Fadeout());
+        }
+
+        /// <summary>
+        /// 存檔完淡出
+        /// </summary>
+        private IEnumerator Fadeout()
+        {
+            groupSaveLoad.interactable = false;
+            groupSaveLoad.blocksRaycasts = false;
+
+            for (int i = 0; i < 10; i++)
+            {
+                groupSaveLoad.alpha -= 0.1f;
+                yield return waitFadeInterval;
+            }
+
+            Time.timeScale = 1;
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        private IEnumerator SetPlayer()
+        {
+            controller.enabled = false;
+            ani.enabled = false;
+            traPlayer.position = player.position;
+            traPlayer.eulerAngles = player.rotation;
+            yield return new WaitForSecondsRealtime(0.5f);
+            controller.enabled = true;
+            ani.enabled = true;
         }
     }
 }
